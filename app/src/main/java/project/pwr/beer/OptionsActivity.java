7250f4efb2a11612e659644a557152fffa68e7c5
188383 <188383@users.noreply.github.com>
2015-05-22 19:51:58 +0200

    Added Registeration, must remove the options button, which is basically useless

--- app/src/main/java/project/pwr/beer/OptionsActivity.java
+++ app/src/main/java/project/pwr/beer/OptionsActivity.java
@@ -1,18 +1,52 @@
 package project.pwr.beer;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class OptionsActivity extends Activity {
+   // public static final String USER = "com.user";
+  //  public static final String PASS = "com.pass";
+
+    public void onPause(){
+        super.onPause();
+    }
+
+    public void onResume(){
+        super.onResume();
+    }
+
+    public void onBackPressed(){
+        Intent intent = new Intent();
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        Button save = (Button)findViewById(R.id.save_but);
+        Button cancel = (Button)findViewById(R.id.cancel_but);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //add checking for name and email address
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //return from registration
+                //with warning that exit to register
+            }
+        });
     }
 }
--- app/src/main/java/project/pwr/database/BeerDataBase.java
+++ app/src/main/java/project/pwr/database/BeerDataBase.java
@@ -1,7 +1,26 @@
 package project.pwr.database;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by pawel on 22.05.15.
+ *
  */
-public class BeerDataBase {
+
+public class BeerDataBase extends SQLiteOpenHelper{
+
+    public BeerDataBase(Context context){
+        super(context,null,null,0);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
